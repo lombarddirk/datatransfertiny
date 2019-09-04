f8fe8940fcc444bac6a413298c3234f39651688b
Dirk <dirk.lombard@standardbank.co.za>
2019-09-04 10:59:18 +0200

    Add trim to strings

--- src/main/java/com/nigel/high/datatransfer/App.java
+++ src/main/java/com/nigel/high/datatransfer/App.java
@@ -11,8 +11,8 @@ public class App
     public static void main( String[] args )
     {
 		ExcelReaderOldFormat excelReaderOldFormat = new ExcelReaderOldFormat();
-		HashMap<String, ReaderPOJO> dataFromSpreadsheet = excelReaderOldFormat.readerExcelSheet(args[0], args[1]);
+		HashMap<String, ReaderPOJO> dataFromSpreadsheet = excelReaderOldFormat.readerExcelSheet(args[0].trim(), args[1].trim());
 		ExcelUpdaterOldFormat excelUpdaterOldFormat =  new ExcelUpdaterOldFormat();
-		excelUpdaterOldFormat.updateExcelSheet(dataFromSpreadsheet, args[4], args[2], args[3]);
+		excelUpdaterOldFormat.updateExcelSheet(dataFromSpreadsheet, args[4].trim(), args[2].trim(), args[3].trim());
     }
 }
